733c1ab40a77a25f7048a79ce402fde31a7d0cb9
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-04-24 16:39:57 -0700

    Navbar Cleaning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,23 @@ import linkedinLogo from "../assets/linkedin.svg"
 import githubLogo from "../assets/github.svg"
 const Navbar = () => {
   return (
-    <div className="w-full xl:w-[1280px]">
-      <div className="flex w-full xl:w-[1280px] mt-5">
-          <a href="#" className= "flex justify-center items-center font-display font-lightb grow-[1] basis-1/5" > @bensikora </a>
+    <div className="">
+      <div className="flex mt-5">
+          <a href="#" className= "flex justify-center items-center font-display text-[1.3rem] grow-[1] basis-1/5" > @bensikora </a>
           <ul className= 
-          "justify-center inline-flex items-center text-lg font-display font-light grow-[3] basis-3/5">
+          "flex justify-center items-center text-[1.3rem] font-display grow-[3] basis-3/5">
               <li className='px-7'><a href="#">About</a></li> 
+              <li className='px-7'><a href="#">Projects</a></li>
               <li className='px-7'><a href="#">Experience</a></li>
               <li className='px-7'><a href="#">Blog</a></li>
-              <li className='px-7'><a href="#">Resume</a></li>
+    
           </ul>
-          <div className="flex grow-[1]justify-center shrink-0 basis-1/5">
-            <img src= {githubLogo} className= "w-[25px] h-[25px] mr-3"/>
-            <img src={linkedinLogo} className= "w-[25px] h-[25px]"/>
+          <div className="flex grow-[1] justify-center shrink-0 basis-1/5">
+            <img src= {githubLogo} className= "w-[35px] h-[35px] mr-3"/>
+            <img src={linkedinLogo} className= "w-[35px] h-[35px]"/>
           </div>
       </div>
-      <hr className="border-t-10 border-black mx-10" />
+      <hr className="border-t-1 border-black mx-10" />
     </div>
   )
 }
