675aa0698a5f307cda8fb2db00f6ac4b65e81845
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-04-28 18:06:58 -0700

    Animation Navbar
    Specifically tackled the logos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import linkedinLogo from "../assets/linkedin.svg"
 import githubLogo from "../assets/github.svg"
+import Styles from "../style.js"
+import GithubComp from "../assets/GithubComp.jsx"
+import LinkedinComp from "../assets/LinkedinComp.jsx"
 
-import githubComp from "../assets/githubComp.jsx"
 const Navbar = () => {
 
   const buttonAnimation='transform hover:-translate-y-1 hover:text-darkblue hover:scale-105 ease-in-out delay-70 duration-300' 
@@ -10,19 +12,18 @@ const Navbar = () => {
   return (
     <div className="">
       <div className="flex mt-5">
-          <a href="#" className= "flex justify-center items-center font-display text-[1.3rem] grow-[1] basis-1/5" > @bensikora </a>
+          <a href="#" className= {`flex justify-center items-center font-display text-[1.3rem] grow-[1] basis-1/5 ${Styles.buttonAnnimation}`} > @bensikora </a>
           <ul className= 
           "flex justify-center items-center text-[1.3rem] font-display grow-[3] basis-3/5">
-              <li className={`px-7 ${buttonAnimation}`}><a href="#">About</a></li> 
-              <li className={`px-7 ${buttonAnimation}`}><a href="#">Projects</a></li>
-              <li className={`px-7 ${buttonAnimation}`}><a href="#">Experience</a></li>
-              <li className={`px-7 ${buttonAnimation}`}><a href="#">Blog</a></li>
+              <li className={`px-7 ${Styles.buttonAnnimation}`}><a href="#">About</a></li> 
+              <li className={`px-7 ${Styles.buttonAnnimation}`}><a href="#">Projects</a></li>
+              <li className={`px-7 ${Styles.buttonAnnimation}`}><a href="#">Experience</a></li>
+              <li className={`px-7 ${Styles.buttonAnnimation}`}><a href="#">Blog</a></li>
     
           </ul>
           <div className="flex grow-[1] justify-center shrink-0 basis-1/5">
-            <githubComp/>
-            {/*<img src= {githubLogo} className= "w-[35px] h-[35px] mr-3"/>*/}
-            <img src={linkedinLogo} className= "w-[35px] h-[35px]"/>
+            <GithubComp />
+            <LinkedinComp />
           </div>
       </div>
       <hr className="border-t-1 border-black mx-10" />
