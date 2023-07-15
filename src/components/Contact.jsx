eb096c430d0616a5295b7ec8df9b0f6ca728214f
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-07-14 17:49:36 -0700

    Fixed Link Issue

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,29 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import paintbrushLightBlue from "../assets/paintbrushLightblueL.svg";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const Contact = () => {
-
-    return (
-      <motion.div 
-      initial={{ opacity: 0, y:-75, x:-10}}
-      whileInView={{ opacity: 1, y:0, x:0}}
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -75, x: -10 }}
+      whileInView={{ opacity: 1, y: 0, x: 0 }}
       viewport={{ once: true }}
-      transition={{ delay: 0.3, duration: 0.8}}>
-    <div className="flex flex-col items-center mb-4" id='contact-page'>
+      transition={{ delay: 0.3, duration: 0.8 }}
+    >
+      <div className="flex flex-col items-center mb-4" id="contact-page">
         <div className="grid grid-rows-1 grid-cols-1 box-border">
-            <img src={paintbrushLightBlue} className='h-auto w-full max-w-[380px] min-w-[320px] sm:max-w-none sm:w-[70%] md:w-[80%] lg:w-[85%] z-0 row-start-1 row-end-2 col-start-1 col-end-2 mx-auto'/>
-            <h1 className="font-writing font-normal text-white text-[2.4rem] md:text-[2.9rem] lg:text-[3.5rem]  z-10 row-start-1 row-end-2 col-start-1 col-end-2 mx-auto mt-2">Get in Touch!</h1>
+          <img
+            src={paintbrushLightBlue}
+            className="h-auto w-full max-w-[380px] min-w-[320px] sm:max-w-none sm:w-[70%] md:w-[80%] lg:w-[85%] z-0 row-start-1 row-end-2 col-start-1 col-end-2 mx-auto"
+          />
+          <h1 className="font-writing font-normal text-white text-[2.4rem] md:text-[2.9rem] lg:text-[3.5rem]  z-10 row-start-1 row-end-2 col-start-1 col-end-2 mx-auto mt-2">
+            Get in Touch!
+          </h1>
         </div>
-        <p className='text-[1.4rem] md:text-[1.8rem]  font-[800] leading-[50px] lg:leading-[60px] text-darkgreen text-center sm:text-left'>I am always open to connect!</p>
-        <p className='text-[1.4rem] md:text-[1.8rem]  font-[800] leading-[50px] lg:leading-[60px] text-darkgreen text-center '>I am currently searching for new oppurtunites.</p>
-        <div className='flex flex-col sm:flex-row text-[1.2rem] md:text-[1.5rem]  font-[800] leading-[50px] lg:leading-[60px] text-darkgreen text-center'>
-        <a href="mailto:bssikora@gmail.com" target="_blank" rel="noopener noreferrer"><p className='underline'>bssikora@gmail.com</p></a>
-            <a href="www.linkedin.com/in/ben-sikora/" target="_blank" rel="noopener noreferrer"><p className='sm:ml-3 underline'>www.linkedin.com/in/ben-sikora/</p></a>
+        <p className="text-[1.4rem] md:text-[1.8rem]  font-[800] leading-[50px] lg:leading-[60px] text-darkgreen text-center sm:text-left">
+          I am always open to connect!
+        </p>
+        <p className="text-[1.4rem] md:text-[1.8rem]  font-[800] leading-[50px] lg:leading-[60px] text-darkgreen text-center ">
+          I am currently searching for new oppurtunites.
+        </p>
+        <div className="flex flex-col sm:flex-row text-[1.2rem] md:text-[1.5rem]  font-[800] leading-[50px] lg:leading-[60px] text-darkgreen text-center">
+          <a
+            href="mailto:bssikora@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="underline">bssikora@gmail.com</p>
+          </a>
+          <a
+            href="//linkedin.com/in/ben-sikora/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="sm:ml-3 underline">www.linkedin.com/in/ben-sikora/</p>
+          </a>
         </div>
       </div>
-      </motion.div>
-    )
-  }
-  
-  export default Contact
+    </motion.div>
+  );
+};
+
+export default Contact;
