05385a3882f572267bc1573ace689001ee66a01a
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-07-21 14:41:46 -0700

    Fixed Navbar Spacing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,10 +20,10 @@ const Navbar = () => {
           <a href="#hero-page" className= {`hidden md:flex justify-center items-center font-display text-[1.3rem] grow-[1] basis-1/5 ${Styles.buttonAnnimation}`} > @bensikora </a>
           <ul className= 
           "hidden md:flex justify-center items-center text-[1.3rem] font-display grow-[3] basis-3/5">
-              <li className={`pr-4 l:pr-7 before:bg-none ${Styles.buttonAnnimation}`}><a href="#about-page">About</a></li> 
-              <li className={`px-4 l:px-7 before:bg-none ${Styles.buttonAnnimation}`}><a href="#projects-page">Projects</a></li>
-              <li className={`px-4 l:px-7 before:bg-none ${Styles.buttonAnnimation}`}><a href="#experience-page">Experience</a></li>
-              <li className={`px-4 l:pl-7 before:bg-none ${Styles.buttonAnnimation}`}><a href="#contact-page">Contact</a></li>
+              <li className={`pr-3 l:pr-7 before:bg-none ${Styles.buttonAnnimation}`}><a href="#about-page">About</a></li> 
+              <li className={`px-3 l:px-7 before:bg-none ${Styles.buttonAnnimation}`}><a href="#projects-page">Projects</a></li>
+              <li className={`px-3 l:px-7 before:bg-none ${Styles.buttonAnnimation}`}><a href="#experience-page">Experience</a></li>
+              <li className={`px-3 l:pl-7 before:bg-none ${Styles.buttonAnnimation}`}><a href="#contact-page">Contact</a></li>
     
           </ul>
           <div className="flex grow-[1] justify-end mr-8 shrink-0 basis-1/5 md:justify-center md:mr-0">
