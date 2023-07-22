f6a305b16a7b383748a51ab3bcf06dda5352b77d
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-07-22 11:32:25 -0700

    Small typo fix

--- src/content.js
+++ src/content.js
@@ -14,7 +14,7 @@ export const contentIndProjects = [
   {
     id: 0,
     content:
-      "My website! This website was fully written/deployed with no templates. The process taught me a great deal about frontend coding and the react framework.",
+      "My website! This website was fully written/deployed with no templates. The process taught me a great deal about frontend coding and the React framework.",
     img: websiteLogo,
     link: "https://github.com/ben-sikora/portfolio-website",
   },
